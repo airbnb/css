d52cda1b226ce7b4a79201b3a63730f1330d8522
Chlorophyllkid <chlorophyllkid@gmail.com>
2017-11-17 17:06:45 +0100

    Removed/changed rules
    removed 'no-invalid-double-slash-comments': true,
    changed 'at-rule-empty-line-before': ['always', { except: ['first-nested'] }]

--- packages/stylelint-config-airbnb/test/test-formatting.js
+++ packages/stylelint-config-airbnb/test/test-formatting.js
@@ -35,7 +35,7 @@ test('Warnings in bad-formatting.css', t => {
 
     t.truthy(errored, 'errored');
 
-    t.is(warnings.length, 10);
+    t.is(warnings.length, 11);
 
     t.is(warnings[0].text, 'Expected indentation of 2 spaces (indentation)');
     t.is(warnings[1].text, 'Expected indentation of 2 spaces (indentation)');
@@ -47,10 +47,11 @@ test('Warnings in bad-formatting.css', t => {
       warnings[6].text,
       'Expected empty line before non-nested rule (rule-non-nested-empty-line-before)'
     );
-    t.is(warnings[7].text, 'Expected single space before "{" (block-opening-brace-space-before)');
-    t.is(warnings[8].text, 'Expected single space after ":" (declaration-colon-space-after)');
+    t.is(warnings[7].text, 'Expected empty line before comment (comment-empty-line-before)');
+    t.is(warnings[8].text, 'Expected single space before "{" (block-opening-brace-space-before)');
+    t.is(warnings[9].text, 'Expected single space after ":" (declaration-colon-space-after)');
     t.is(
-      warnings[9].text,
+      warnings[10].text,
       'Unexpected value "none" for property "border" (declaration-property-value-blacklist)'
     );
   });
